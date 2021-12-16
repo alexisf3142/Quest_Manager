096af5408bafb9f740a57c981d0fc6d4d6921b44
John-Proctor <proctor_john@wheatoncollege.edu>
2021-12-16 10:59:41 -0500

    Comments and removing cheats

--- app/src/main/java/com/example/questmanager/DungeonActivity.java
+++ app/src/main/java/com/example/questmanager/DungeonActivity.java
@@ -32,20 +32,22 @@ public class DungeonActivity extends AppCompatActivity {
     Monster dragon; //= new Monster("dragon",10,25);
     Monster washingMachine; //=new Monster("washingMachine",5,10);
     Monster martin; //= new Monster("martin",12,40);
-    Monster ghost;
-    Monster tree;
-    Monster goblin;
-    Monster werewolf;
+    Monster ghost; //= new Monster("ghost",6,20)
+    Monster tree;//= new Monster("tree",8,23)
+    Monster goblin;//= new Monster("goblin",3,10)
+    Monster werewolf;//= new Monster("werewolf",9,19)
     Monster curMonster; //= null;
     Character playerCharacter;// = readFile();
     Random r = new Random(System.currentTimeMillis());
-    int mainStat = 0;
+    int mainStat = 0; // main combat stat depending on class
     int dunLevel = 1; // int counter to keep track of the dungeon level
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dungeon);
+
+        //Propagates all of the monster classes.
         slime = new Monster("slime", 4, 10,R.drawable.slime_monster,R.drawable.dead_slime_monster);
         toothBrush = new Monster("goblin", 3, 15,R.drawable.toothbrush_monster,R.drawable.dead_toothbrush_monster);
         waterCup = new Monster("water cup", 6, 5,R.drawable.water_monster,R.drawable.dead_water_monster);
@@ -58,16 +60,17 @@ public class DungeonActivity extends AppCompatActivity {
         werewolf = new Monster("werewolf",9,19,R.drawable.werewolf_monster,R.drawable.dead_werewolf_monster);
 
 
-        curMonster = slime;
-        Intent infoIntent = getIntent();
-        playerCharacter = (Character) infoIntent.getSerializableExtra("Character");
-        readFile();
-        getMainStat();
-        setMonProgress();
-        setHerProgress();
+        curMonster = slime; // starts with basic monster
+        readFile(); // read file to get Character info
+        getMainStat(); //finds main combat stat
+        setMonProgress(); //sets monster health bar
+        setHerProgress(); //sets hero health bar
 
     }
 
+    /**
+     * This function checks the class and gets whatever stat helps determine damage
+     */
     public void getMainStat(){
         String profession = playerCharacter.getProfession();
         if (profession.equals("Knight")){
@@ -81,6 +84,9 @@ public class DungeonActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This reads in the character file
+     */
     public void readFile() {
         File playerData = getBaseContext().getFileStreamPath("playerData.txt");
         //looks for the file to which player character data is saved
@@ -139,6 +145,8 @@ public class DungeonActivity extends AppCompatActivity {
      * @param view
      */
     public void continueButton(View view) {
+
+        //Code below here manages changing from into screen to combat screen.
         LinearLayout actionLayout = findViewById(R.id.actionLinearLayout);
         LinearLayout initialLayout = findViewById(R.id.initialLinearLayout);
         TextView titleOne = findViewById(R.id.titleOneTextView);
@@ -153,14 +161,24 @@ public class DungeonActivity extends AppCompatActivity {
 
 
 
+        //changes header
         titleOne.setText("Dungeon Level");
         titleTwo.setText(String.valueOf(dunLevel));
-        generateMonster();
-        setMain();
-        setMonProgress();
+        generateMonster(); //generates the monster
+        setMain(); //sets the main flavor text
+        setMonProgress(); // sets the monster health bar.
     }
 
+    /**
+     * This button does the attack.
+     * When ever the character makes an attack the enemy also takes an action through a function
+     * call to monstermove().There is also a function call to checkDeath() to see if the character
+     * or monster have died.
+     * @param view
+     */
     public void attackButton(View view) {
+
+        // code below here sets up the combat tracker
         TextView flavorText = findViewById(R.id.mainTextView);
         TextView heroTitle = findViewById(R.id.heroTextView);
         TextView heroAction = findViewById(R.id.heroAction);
@@ -185,6 +203,7 @@ public class DungeonActivity extends AppCompatActivity {
 
         String move = monsterMove();
 
+        //if else loop to see what attack the monster is doing so the proper operations are being done
         if (move.equals("attack")) {
             monAction.setText("Attack");
             int monDmg = r.nextInt(curMonster.getMonDmg());
@@ -220,7 +239,16 @@ public class DungeonActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This button makes the character defend.
+     * When ever the character defends the enemy also takes an action through a function
+     * call to monstermove().There is also a function call to checkDeath() to see if the character
+     * or monster have died.
+     * @param view
+     */
     public void defendButton(View view) {
+
+        //sets up combat tracker.
         TextView flavorText = findViewById(R.id.mainTextView);
         TextView heroTitle = findViewById(R.id.heroTextView);
         TextView heroAction = findViewById(R.id.heroAction);
@@ -236,6 +264,7 @@ public class DungeonActivity extends AppCompatActivity {
 
         String move = monsterMove();
 
+        // if else to check what action the enemy is doing and then executing that action.
         if (move.equals("attack")) {
             boolean chargedAttack = false;
             monAction.setText("Attack");
@@ -271,8 +300,16 @@ public class DungeonActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This button makes the character charge an attack.
+     * When ever the character charges an attack the enemy also takes an action through a function
+     * call to monstermove(). There is also a function call to checkDeath() to see if the character
+     * or monster have died.
+     * @param view
+     */
     public void chargeButton(View view) {
 
+        //sets up the combat tracker
         TextView flavorText = findViewById(R.id.mainTextView);
         TextView heroTitle = findViewById(R.id.heroTextView);
         TextView heroAction = findViewById(R.id.heroAction);
@@ -287,6 +324,8 @@ public class DungeonActivity extends AppCompatActivity {
         monAction.setVisibility(View.VISIBLE);
 
         String move = monsterMove();
+
+        // set of if statements to see what the enemy is doing and executing that action.
         playerCharacter.setCharged(true);
         if (move.equals("attack")) {
             monAction.setText("Attack");
@@ -315,15 +354,23 @@ public class DungeonActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This function checks wether the character or monster have died. If the character has died
+     * then it sends the user back to the home screen. If the monster dies then it moves the user to
+     * a defeated monster screen.
+     */
     private void checkDeath() {
         double goldGained = playerCharacter.getGold() ;
+        // if player dies sends to home screen
         if (playerCharacter.getCurpower() <= 0) {
-            playerCharacter.setCharged(false);
-            playerCharacter.setCurpower(0);
-            updateCharacterFile();
+            playerCharacter.setCharged(false); //makes sure you can't bank charged attacks
+            playerCharacter.setCurpower(0); // doesn't let the player have negative health
+            updateCharacterFile(); // updates char file
             returnMainScreen();
             Toast.makeText(getApplicationContext(), "Nice try adventurer... better luck next time", Toast.LENGTH_LONG).show();
-        } else if (curMonster.getMonCurPow() <= 0) {
+        }
+        // if the monster dies gives players gold and gets new monster
+        else if (curMonster.getMonCurPow() <= 0) {
 
             if(curMonster==martin){
                 goldGained = playerCharacter.getGold() + 20 + 5 * playerCharacter.getLck()/20.0;}
@@ -340,20 +387,21 @@ public class DungeonActivity extends AppCompatActivity {
 
     }
 
-
+    /**
+     * This function generates the next monster for the user to fight.
+     */
     private void generateMonster() {
 
+        // num used to select random monster
         int num = r.nextInt(9);
         ImageView monsterIV = findViewById(R.id.monsterView);
         curMonster.setCharged(false);
         curMonster.setCurPow(curMonster.getMonMaxPow());
-        if (dunLevel % 3 == 0) {
+        // tracks wether to display boss fight
+
+        if (dunLevel % 10 == 0) {
             curMonster = martin;
-            monsterIV.setImageResource(curMonster.getPic());
             setMonProgress();
-            //if (dunLevel % 10 == 0) {
-            //curMonster = martin;
-            //setMonProgress();
         } else {
             if (num == 0) {
                 curMonster = slime;
@@ -385,7 +433,6 @@ public class DungeonActivity extends AppCompatActivity {
 
     /**
      * sends the user back to the main screen.
-     *
      * @param view
      */
     public void buttonMainScreen(View view) {
@@ -393,11 +440,18 @@ public class DungeonActivity extends AppCompatActivity {
         startActivity(mainScreenIntent);
     }
 
+    /**
+     * This also ends the user back to the main screen, but it isn't meant to be used on a button,
+     * but is called when the user dies.
+     */
     public void returnMainScreen() {
         Intent mainScreenIntent = new Intent(this, MainActivity.class);
         startActivity(mainScreenIntent);
     }
 
+    /**
+     * This function sets the monster health bar.
+     */
     public void setMonProgress() {
         ProgressBar enemyHealth = findViewById(R.id.enemyHealthBar);
         enemyHealth.setMax(100);
@@ -407,6 +461,9 @@ public class DungeonActivity extends AppCompatActivity {
         enemyHealth.setProgress(progress);
     }
 
+    /**
+     * This function sets the hero health bar.
+     */
     public void setHerProgress() {
         ProgressBar heroHealth = findViewById(R.id.heroHealthBar);
         heroHealth.setMax(100);
@@ -416,6 +473,9 @@ public class DungeonActivity extends AppCompatActivity {
         heroHealth.setProgress(progress);
     }
 
+    /**
+     * This function selects the proper flavor text to display to the user.
+     */
     public void setMain(){
         TextView mainTextView = findViewById(R.id.mainTextView);
         TextView heroTitle = findViewById(R.id.heroTextView);
@@ -428,8 +488,11 @@ public class DungeonActivity extends AppCompatActivity {
         monAction.setVisibility(View.GONE);
 
         mainTextView.setVisibility(View.VISIBLE);
+
+        //random number to have random flavor text
         int num = r.nextInt(9);
 
+        // tracks whether to display boss screen or not
         if(curMonster == martin){
             int resourceId = this.getResources().getIdentifier("@string/level_boss", "string", this.getPackageName());
             mainTextView.setText(resourceId);
@@ -479,6 +542,10 @@ public class DungeonActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * This function determines what sort of move the monster is going to make during combat.
+     * @return A string with the name of the attack.
+     */
     private String monsterMove(){
 
         String Move;
@@ -493,7 +560,11 @@ public class DungeonActivity extends AppCompatActivity {
         return Move;
     }
 
+    /**
+     * This is the death screen that appears when a monster is killed.
+     */
     public void deathScreen(){
+        //Manages displaying or hiding all of the required text views.
         TextView heroTitle = findViewById(R.id.heroTextView);
         TextView monTitle = findViewById(R.id.monsterTextView);
         TextView heroAction = findViewById(R.id.heroAction);
@@ -507,6 +578,7 @@ public class DungeonActivity extends AppCompatActivity {
         ImageView monIV = findViewById(R.id.monsterView);
         LinearLayout combatView = findViewById(R.id.actionLinearLayout);
         LinearLayout initialView = findViewById(R.id.initialLinearLayout);
+        //shows whatever death screen is intended.
         if(curMonster == martin){
             int resourceId = this.getResources().getIdentifier("@string/boss_end", "string", this.getPackageName());
             flavorText.setText(resourceId);
